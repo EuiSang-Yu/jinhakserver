a6d1f44ae66f3943c08828238fe2d1fdc499b736
EuiSang-Yu <dmltkd1974@naver.com>
2021-02-03 16:50:36 +0900

    0203_Redux패턴 서버 진행중

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var express = require("express");
+const express = require("express");
 const path = require("path");
-var bodyParser = require("body-parser");
-// var URL = require("url");
-// var moment = require("moment");
+const bodyParser = require("body-parser");
+const api = require("./routes");
+const app = express();
+const port = process.env.PORT || 3001;
 
-var api = require("./routes");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "hello express" });
+});
+
+app.listen(port, () => console.log(`${port} 번 포트 성공`));
