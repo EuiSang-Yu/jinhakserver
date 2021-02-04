9c3fe2165ccd1e97aa7592390494c521ce140d31
EuiSang-Yu <dmltkd1974@naver.com>
2021-02-04 13:35:17 +0900

    서버에서 데이터받아서 출력까지 완료

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 const app = express();
 const api = require("./routes/index");
-const cors = require("cors");
 const bodyParser = require("body-parser");
-const router = express.Router();
 
-app.use("/api", api);
 app.use(cors());
-
-app.use(bodyParser.json());
-
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "hello express" });
-});
+app.use("/api", api);
 
 app.listen(3001, () => console.log("3001 번 포트 성공"));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const users = [
+  { name: "홍길동", age: "25" },
+  { name: "고길동", age: "35" },
+  { name: "박길동", age: "45" }
+];
+
+// router.get("/", (req, res) => {
+//   console.log("http://localhost:3001/api/");
+//   res.send({ title: "프론트로 데이터 넘기기성공!" });
+// });
+
 router.get("/", (req, res) => {
   console.log("http://localhost:3001/api/");
-  res.send({ title: "hello react!" });
+  res.send(users);
 });
 
 module.exports = router;
